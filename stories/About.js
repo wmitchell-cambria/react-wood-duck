9c7c9c1cc45c44d95b3e06723163178fd8e1b8c6
Melbourne <30814094+mlipardo@users.noreply.github.com>
2017-10-03 16:28:36 -0700

    Added storybook and dependencies (#4)
    * Added storybook and dependencies
    
    * Add Exporting Storybook as a static app
    
    * Fixed spelling character
    
    * Remove storybook output from vcs
    
    * Ignore storybook build output from VCS
    
    * Add storybook deploy helper
    
    * Added nested component on listItem and fixed dropdown
    
    * Fixed drop down

---
+++ stories/About.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const About = () => (
+  <main className="styleguide-content">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="styleguide-inner">
+            <h1 className="styleguide">CWDS Pattern Library</h1>
+
+            <p className="lead">Version 0.31</p>
+
+            <p className="lead">
+              This pattern library is for all the digital services to contribue
+              to and use for CWDS. Current library is CSS only but our goal is
+              to have a component-based library. This is a cross team
+              collaborative effort between designer and developers on Intake,
+              CALS, and Case Managment. It is open source and the repository can
+              be found at:{" "}
+              <a href="">https://github.com/ca-cwds/research-design</a>
+            </p>
+
+            <p className="lead">Learn more about:</p>
+
+            <ul>
+              <li>
+                <a href="https://github.com/ca-cwds/research-design/wiki/pattern-library">
+                  Pattern library vision and goals
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/ca-cwds/research-design/wiki/Experience-Values">
+                  User Experience Values
+                </a>{" "}
+                we design to.
+              </li>
+              <li>
+                <a href="https://github.com/ca-cwds/research-design/wiki/Accessible-Design">
+                  Accessibilty Standards
+                </a>{" "}
+                we follow.
+              </li>
+              <li>
+                <a href="https://github.com/ca-cwds/research-design/wiki/Technical-Design-Brief">
+                  Browsers and screens
+                </a>{" "}
+                we support.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+);
+
+export default About;
