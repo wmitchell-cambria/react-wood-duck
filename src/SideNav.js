a1a5a0b24c3784b638284bc750a8c2a18d78f42a
Sneha Nagole <snagole@bitbucket.org>
2017-10-03 18:42:36 -0700

    First iteration of Side Nav with tests

---
+++ src/SideNav.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class SideNav extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		const classField = `container col-md-${this.props.columnWidth}`;
+		if(!this.props.content) { 
+			return null;
+		} else { 
+			return (
+	         		<div className={classField}>
+					<div className="row">
+						<ul className="nav nav-stacked">
+							<li className="active"><a href="#">Test Item 0</a></li>
+        	       					<li><a href="#">Test Item 1</a></li>
+        	     					<li><a href="#">Test Item 2</a></li>
+		    				</ul>
+					</div>
+				</div>
+			);
+		}
+	}
+}
+
+SideNav.PropTypes = {
+	content: PropTypes.string,
+	columnWidth: PropTypes.number
+}
+
+SideNav.defaultProps = {
+	content: "",
+	columnWidth: 3
+}
+
+module.exports = SideNav;
