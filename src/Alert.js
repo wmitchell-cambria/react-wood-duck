235f7a2f059262d39d3b885239b4659f05a094f4
Sushmareddy-OSI <sushma.kalvakuntla@osi.ca.gov>
2018-02-23 10:16:04 -0800

    modified alert with messageData()

--- src/Alert.js
+++ src/Alert.js
@@ -4,20 +4,28 @@ import PropTypes from 'prop-types';
 const Alert = props => {
   const classField = `alert-message ${props.alertClassName}-message`;
   const faIcons = `fa ${props.faIcon}`;
-
+  let messageData = () => {
+    if (Array.isArray(props.alertMessage)) {
+      return props.alertMessage.map((item, i) => <p key={i}> {item}</p>);
+    } else {
+      return props.alertMessage;
+    }
+  };
   return (
-    <div className="row">
-      <div className="col-xs-12">
-        <div className={classField}>
-          <div className="alert-icon">
-            <i className={faIcons} />
-          </div>
-          <div className="alert-text">{props.alertMessage}</div>
-          {props.alertCross && (
-            <div className="alert-cross">
-              <i className="fa fa-times" />
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-xs-12">
+          <div className={classField}>
+            <div className="alert-icon">
+              <i className={faIcons} />
             </div>
-          )}
+            <div className="alert-text">{messageData()}</div>
+            {props.alertCross && (
+              <div className="alert-cross">
+                <i className="fa fa-times" />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
@@ -27,7 +35,7 @@ const Alert = props => {
 Alert.propTypes = {
   alertClassName: PropTypes.string,
   faIcon: PropTypes.string,
-  alertMessage: PropTypes.string,
+  alertMessage: PropTypes.any,
   alertCross: PropTypes.bool,
 };
 
